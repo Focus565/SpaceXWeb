5d6510f42fe31335f373b7864940442a10c48b04
focus <fogus251@hotmail.com>
2021-02-18 14:36:34 +0700

    fetch api in launch page

--- spacexweb/src/Pages/Launches.js
+++ spacexweb/src/Pages/Launches.js
@@ -1,10 +1,24 @@
+import { useState, useEffect } from 'react';
+// import  DataGrid  from '@material-ui/data-grid';
 
 
 
-const Launches = (props) => {
+const Launches = () => {
     // console.log();
+    const [launches, setlaunches] = useState([])
+    useEffect(
+        () => {
+            const fetchLanuchs = async () => {
+                const response = await fetch("https://api.spacexdata.com/v3/launches")
+                const data = await response.json()
+                setlaunches(data)
+            }
+            fetchLanuchs()
+        }
+    )
     return (
-        <div>Launches</div>
+        <div>{JSON.stringify(launches,null,2)}</div>
+        // <div>{launches.map((launch) =>(<li>{launch.mission_name}</li>))}</div>
     )
 }
 
