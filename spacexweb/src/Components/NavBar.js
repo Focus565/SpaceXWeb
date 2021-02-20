0226a15de3edfa4b463571014bb8d6800b9a5782
Wasitpol Jongmanasjaroean <41641716+RewiKaze@users.noreply.github.com>
2021-02-20 20:06:22 +0700

    Update Design

--- spacexweb/src/Components/NavBar.js
+++ spacexweb/src/Components/NavBar.js
@@ -1,45 +1,50 @@
-import { Container } from '@material-ui/core';
-import {
-    // BrowserRouter as Router,
-    NavLink
-} from 'react-router-dom'
-
+import { Container } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
 
 const styleNavBar = {
-    position: 'fixed',
-    background: "rgba(209,209,209,0.43)",
-    height: "4em",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    top:"0"
-}
+  position: "fixed",
+  background: "rgba(209,209,209,0.43)",
+  height: "4em",
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  top: "0",
+};
 const linkSelected = {
-    color: "grey",
-    textDecoration: "none"
-}
+  color: "grey",
+  textDecoration: "none",
+  cursor: "default",
+};
 
-
+const linkStyled = {
+  color: "black",
+  textDecoration: "none",
+  margin: "1em",
+  fontSize: "1.5vw",
+};
 
 const NavBar = () => {
-    return (
-        <>  
-            <Container maxWidth="xl" style={styleNavBar}>
-                <NavLink exact to="/" activeStyle={linkSelected} >
-                        Home
-                    </NavLink>
-                    <NavLink to="/Rockets" activeStyle={linkSelected}>
-                        Rockets
-                    </NavLink>
-                    <NavLink to="/Launches" activeStyle={linkSelected}>
-                        Launches
-                    </NavLink>
-
-            </Container>
-        </>
-    )
-}
-
-
+  return (
+    <>
+      <Typography>
+        <Container maxWidth="xl" style={styleNavBar}>
+          <NavLink to="/">
+            <img src="images/logo.png" style={{ width: "15vw" }} />
+          </NavLink>
+          <NavLink exact to="/" activeStyle={linkSelected} style={linkStyled}>
+            Home
+          </NavLink>
+          <NavLink to="/Rockets" activeStyle={linkSelected} style={linkStyled}>
+            Rockets
+          </NavLink>
+          <NavLink to="/Launches" activeStyle={linkSelected} style={linkStyled}>
+            Launches
+          </NavLink>
+        </Container>
+      </Typography>
+    </>
+  );
+};
 
 export default NavBar;
--- spacexweb/src/Pages/Home.js
+++ spacexweb/src/Pages/Home.js
@@ -1,38 +1,75 @@
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 
 const HomePage = (
-    <>
-        <Container maxWidth="xl">      
-            <div className="headerName" style={{height:"calc(100vh - 4em)", width:'100%', display:"flex" ,justifyContent:"center" , alignItems:"center",background:"grey", marginTop:"4em" }}>
-                <Typography variant="h1" align="center" >
-                SpaceX
-                </Typography>
-            </div>
-        </Container>
-        <Container style={{height:"100vh", display:"flex" ,justifyContent:"center" , alignItems:"center"}}>
-        <Grid container direction="column">
-            {/* <div className="informationSpaceX" style={{ height: "100vh", display: "flex", justifyContent: "left", alignItems: "center" }}> */}
-                <Grid item lg={6} sm={12}>
-                    <Typography variant="h2" style={{textAlign:"center"}}>
-                        What is SpaceX?
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <Typography variant="body1">
-                        <strong>Space Exploration Technologies Corp. (SpaceX) </strong>is an American aerospace manufacturer and space transportation services company headquartered in Hawthorne, California. It was founded in 2002 by Elon Musk with the goal of reducing space transportation costs to enable the colonization of Mars. SpaceX has developed several launch vehicles and rocket engines, as well as the Dragon cargo spacecraft and the Starlink satellite constellation (providing internet access), and has flown humans and cargo to the International Space Station on the SpaceX Dragon 2.
-                    </Typography>
-                </Grid>
-            {/* </div> */}
+  <>
+    <Container maxWidth="xl">
+      <div
+        className="headerName"
+        style={{
+          height: "calc(100vh - 4em)",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "url('images/bg01.jpg')",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          marginTop: "4em",
+          filter: "blur(2.5px)",
+        }}
+      ></div>
+      <Typography
+        variant="h1"
+        align="center"
+        style={{
+          color: "white",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        SpaceX
+      </Typography>
+    </Container>
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Grid container direction="column">
+        {/* <div className="informationSpaceX" style={{ height: "100vh", display: "flex", justifyContent: "left", alignItems: "center" }}> */}
+        <Grid item lg={6} sm={12}>
+          <Typography variant="h2" style={{ textAlign: "center" }}>
+            What is SpaceX?
+          </Typography>
         </Grid>
-        </Container>
-    </>
-)
-
+        <Grid item>
+          <Typography variant="body1">
+            <strong>Space Exploration Technologies Corp. (SpaceX) </strong>is an
+            American aerospace manufacturer and space transportation services
+            company headquartered in Hawthorne, California. It was founded in
+            2002 by Elon Musk with the goal of reducing space transportation
+            costs to enable the colonization of Mars. SpaceX has developed
+            several launch vehicles and rocket engines, as well as the Dragon
+            cargo spacecraft and the Starlink satellite constellation (providing
+            internet access), and has flown humans and cargo to the
+            International Space Station on the SpaceX Dragon 2.
+          </Typography>
+        </Grid>
+        {/* </div> */}
+      </Grid>
+    </Container>
+  </>
+);
 
 const Home = () => {
-    return (HomePage)
-}
+  return HomePage;
+};
 
 export default Home;
