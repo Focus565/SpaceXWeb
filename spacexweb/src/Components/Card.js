6ee395b004ad3f42bdb8f25e1bce756d50004656
RewiKaze <lnwkazeazelass@hotmail.co.th>
2021-02-21 16:16:12 +0700

    Update Design

--- spacexweb/src/Components/Card.js
+++ spacexweb/src/Components/Card.js
@@ -2,26 +2,55 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Chip from "@material-ui/core/Chip";
-import {Link} from "react-router-dom";
-import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { Typography } from "@material-ui/core";
+
 const Cards = (props) => {
   if (props.var == null) {
     return <div></div>;
   } else {
     if (props.page === "Rocket") {
       return (
-        <Card>
-          <CardMedia
-            style={{ height: 200, width: 200 }}
-            image={props.var.flickr_images}
-          />
-          <CardContent>
-            <h2>{props.var.rocket_name}</h2>
-            {props.var.description}<div></div>
-            <Link to={{pathname:"/RocketDetail",state:{id:props.var.rocket_id}}}> 
-            <Button variant="contained" color="primary">more </Button>
-            </Link>
-          </CardContent>
+        <Card
+          style={{
+            flex: "0 0 calc(100% / 4)",
+            margin: "1em",
+            textAlign: "center",
+          }}
+        >
+          <Typography>
+            <CardMedia
+              style={{
+                height: "50vw",
+                width: "90%",
+                margin: "auto",
+                marginTop: "1em",
+                borderRadius: "10px",
+                backgroundPosition: "center",
+              }}
+              image={props.var.flickr_images}
+            />
+            <CardContent>
+              <h1>{props.var.rocket_name}</h1>
+              {props.var.description}
+              <div></div>
+              <Link
+                to={{
+                  pathname: "/RocketDetail",
+                  state: { id: props.var.rocket_id },
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ margin: "1em" }}
+                >
+                  more{" "}
+                </Button>
+              </Link>
+            </CardContent>
+          </Typography>
         </Card>
       );
     }
@@ -35,20 +64,37 @@ const Cards = (props) => {
           }}
         >
           <CardMedia
-            style={{ height: 200, width: 200 }}
+            style={{ height: "10vw", width: "10vw", margin: "auto" }}
             image={props.var.links.mission_patch}
           />
           <CardContent>
-            <h2>{props.var.mission_name}</h2>
-            <Chip label={props.var.launch_year} />
-            <Chip label={props.var.rocket.rocket_name} />
-            <Chip
-              label={props.var.launch_success == true ? "success" : "failed"}
-            />
+            <Typography>
+              <h2>{props.var.mission_name}</h2>
+              <span>
+                Launch Year : {"   "}
+                <Chip label={props.var.launch_year} />
+                <br />
+                Rocket Name : <Chip label={props.var.rocket.rocket_name} />
+                <br />
+                Launch Success :
+                <Chip
+                  label={
+                    props.var.launch_success == true ? "success" : "failed"
+                  }
+                />
+              </span>
+            </Typography>
           </CardContent>
-          <Link to={{pathname:"/LaunchDetail",state:{id:props.var.flight_number}}}> 
-            <Button variant="contained" color="primary">more </Button>
-            </Link>
+          <Link
+            to={{
+              pathname: "/LaunchDetail",
+              state: { id: props.var.flight_number },
+            }}
+          >
+            <Button variant="contained" color="primary">
+              more{" "}
+            </Button>
+          </Link>
         </Card>
       );
     }
--- spacexweb/src/Pages/Rockets.js
+++ spacexweb/src/Pages/Rockets.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
-import Cards from '../Components/Card';
+import { Container } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import Cards from "../Components/Card";
 
 const Rockets = () => {
-    const [rockets, setRockets] = useState([])
-    useEffect(
-        () => {
-            const fetchRockets = async () => {
-                const response = await fetch("https://api.spacexdata.com/v3/rockets")
-                const data = await response.json()
-                setRockets(data)
-            }
-            fetchRockets()
-        }
-    ,[])
-    return (
-        // <div>{JSON.stringify(rockets,null,2)}</div>
-    <div style={{marginTop:"4em"}}>
+  const [rockets, setRockets] = useState([]);
+  useEffect(() => {
+    const fetchRockets = async () => {
+      const response = await fetch("https://api.spacexdata.com/v3/rockets");
+      const data = await response.json();
+      setRockets(data);
+    };
+    fetchRockets();
+  }, []);
+  return (
+    <Container maxWidth="xl">
+      <div style={{ marginTop: "5em" }}>
         {rockets.map((rocket) => (
-            <Cards var={rocket} page={'Rocket'}/>
+          <Cards var={rocket} page={"Rocket"} />
         ))}
-    </div>
-    )
-}
+      </div>
+    </Container>
+  );
+};
 
 export default Rockets;
